fa3f8c4794b589ba2465c857c86dd5b3d5e123e2
AmanKaushik77 <kaushikaman166@gmail.com>
2022-09-06 12:42:20 -0700

    navbar part 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo-2.png';
 import './App.css';
+import NavBar from './NavBar';
 
 
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
