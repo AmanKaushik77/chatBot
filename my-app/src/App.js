4df1bdd69fe123d291dc21ade8b6cdf89a5c7dcf
AmanKaushik77 <kaushikaman166@gmail.com>
2022-10-01 23:03:23 -0700

    Added components, changed name of site, changed favicon, and fixed formatting bug

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,27 +2,43 @@
 import './App.css';
 import NavBar from './NavBar';
 import Home from './Home';
-import Why from './pages/Why';
-import About from './pages/About';
-
+import Why from './Why';
+import About from './About';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
 
 
 
 function App() {
-  let component
-  switch(window.location.pathname){
-    case "/":
-      component = <Home />;break
-    case "/Why": 
-      component = <Why />; break
-    case "/About": 
-      component = <About />; break
-  }
+  //let component
+  // switch(window.location.pathname){
+  //   case "/":
+  //     component = <Home />;break
+  //   case "/Why": 
+  //     component = <Why />; break
+  //   case "/About": 
+  //     component = <About />; break
+  // }
   return( 
-    <>
-    <NavBar /> {component}
-    
-    </>
+    <Router>
+      <NavBar/>
+        <Routes>
+
+          <Route path="/" element={<Home />} />
+            <Route
+            path="Why"
+            element={
+                <Why/>
+            }
+            />
+            <Route
+            path="About"
+            element={
+                <About/>
+            }
+            />
+        </Routes>
+    </Router>
     );
 }
 
