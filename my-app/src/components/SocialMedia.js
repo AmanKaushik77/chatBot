e038cecd6678a7294117e8aceadc0579243a29f3
AmanKaushik77 <kaushikaman166@gmail.com>
2022-10-05 18:11:28 -0700

    Added social media buttons (done)

---
+++ my-app/src/components/SocialMedia.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import Header from './components/Header';
+import 'C:/Users/kaush/OneDrive/Documents/GitHub/chatBot/my-app/src/styles.css';
+
+
+
+ function SocialMedia (){
+        return (
+        <div className="Photos">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> </link>
+            
+            
+            <a href="#" class="fa fa-facebook"></a>
+            <a href="#" class="fa fa-twitter"></a>
+        </div>
+        )
+    }
+    export default SocialMedia;
