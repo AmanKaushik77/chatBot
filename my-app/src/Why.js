bea5c83d3e62dd6410e9af18c6e6ddc309d4676c
Nicodes1 <nichov01@gmail.com>
2022-10-05 21:23:35 -0600

    #Why is now running and excellent

--- my-app/src/Why.js
+++ my-app/src/Why.js
@@ -8,6 +8,11 @@ export default function Why (){
         return (
             <div className = "about">
                 <Header title = 'Why?'/>
+                <h1> Why make a J.A.R.V.I.S. chatBot?</h1>
+                <p> Well, as a group of 3 friends emerging in the computer science industry
+                     wanting to express their <strong>love</strong> for <em>programming, creating, and expressing</em> a part of who we are.
+                     What better way then creating a website chatBot together! That implements and is themed around a marvel character.
+                     Therefore, we present to you the <b>J.A.R.V.I.S. MCU chat bot!</b></p>
             </div>
         )
     }
